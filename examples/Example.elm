dc2a68ff9a4d0e69369a5fb4b2b0944ae93a7730
Evan Czaplicki <evancz@users.noreply.github.com>
2016-11-15 15:35:10 -0800

    Add comment that points people to URL parsers

--- examples/Example.elm
+++ examples/Example.elm
@@ -38,6 +38,13 @@ type Msg
   = UrlChange Navigation.Location
 
 
+{- We are just storing the location in our history in this example, but
+normally, you would use a package like evancz/url-parser to parse the path
+or hash into nicely structured Elm values.
+
+    <http://package.elm-lang.org/packages/evancz/url-parser/latest>
+
+-}
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
