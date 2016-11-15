0ea228e31e623c5b15aca97915dabeea9443de09
Evan Czaplicki <evancz@users.noreply.github.com>
2016-11-15 15:27:57 -0800

    Update example for 0.18 and make much simpler
    Fix #19 and fix #22 as well. The new example is primarily focused on
    setting up a program with Navigation.program. I think from there, it’ll
    be easier to work out how things work.

---
+++ examples/Example.elm
@@ -0,0 +1,71 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Navigation
+
+
+
+main =
+  Navigation.program UrlChange
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = (\_ -> Sub.none)
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { history : List Navigation.Location
+  }
+
+
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+  ( Model [ location ]
+  , Cmd.none
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = UrlChange Navigation.Location
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    UrlChange location ->
+      ( { model | history = location :: model.history }
+      , Cmd.none
+      )
+
+
+
+-- VIEW
+
+
+view : Model -> Html msg
+view model =
+  div []
+    [ h1 [] [ text "Pages" ]
+    , ul [] (List.map viewLink [ "bears", "cats", "dogs", "elephants", "fish" ])
+    , h1 [] [ text "History" ]
+    , ul [] (List.map viewLocation model.history)
+    ]
+
+
+viewLink : String -> Html msg
+viewLink name =
+  li [] [ a [ href ("#" ++ name) ] [ text name ] ]
+
+
+viewLocation : Navigation.Location -> Html msg
+viewLocation location =
+  li [] [ text (location.pathname ++ location.hash) ]
